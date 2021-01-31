a53714676e80ec2907a060c4f66aba450010389e
Jordan Hickman <jdhickman816@outlook.com>
2021-01-30 19:55:23 -0600

    completed fruit loops 2 problem

---
+++ CSF1Homework5/FruitLoops2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework5
+{
+    class FruitLoops2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Fruit Loops 1");
+            Console.Title = "Fruit Loops 1";
+
+
+            //Declare and initialize an array of any six fruit names (as text). Set up a FOR loop and iterate through them (displaying each value
+            //in the console.)
+            string[] fruits = new string[6]; //declare
+            fruits[0] = "banana";
+            fruits[1] = "apple";
+            fruits[2] = "orange";
+            fruits[3] = "grapefruit";
+            fruits[4] = "strawberry";
+            fruits[5] = "blueberry";
+
+
+            Console.WriteLine("Here are our fruit selections!");
+
+            string index = "";
+            foreach (string fruit in fruits)
+            {
+
+                Console.WriteLine(fruit);
+                index += ".";
+            }
+            Console.WriteLine(index);
+
+        }//End Main()
+    }//End Class
+}//End Namespace
