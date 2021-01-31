a863f6bdad9e582241ef78278f7659f5a1d261b9
Jordan Hickman <jdhickman816@outlook.com>
2021-01-31 15:50:01 -0600

    Login problem started

---
+++ CSF1Homework5/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework5
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * 1. Ask the user for their username
+             * 2. If it matches your one hard-coded, correct username, tell the user that they have access. If not, 
+             * tell the user they have been denied access.
+             * 
+             * 3. Make sure the usernames (hard-coded & user input) are case insensitive (uppercase or lowercase)
+             * ADDITIONAL FUNCTIONALITY:
+             * After the above default functionality is completed:
+             * 4.Encase the username functinoality in a loop that allows the user to make another attempt if the
+             * previous attempt was unsuccessful.
+             * 5.Extend the loop functionality to deny access to the user if they enter the wrong username 3 times.
+             * 6.Extend updated username functionality to ask for a password if the user entered the correct username.
+             * Deny access if the password is entered incorrectly 3 times. (Note: 3 attempts at username, if thke correct
+             * usernam is entered, give 3 attempts for the correct password.
+             */
+        
+            Console.WriteLine("Login");
+            Console.Title = "Login";
+
+            Console.WriteLine("Please input your user name:");
+
+
+        }
+    }
+}
